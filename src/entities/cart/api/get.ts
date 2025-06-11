737092797ee6e140ec8c53c155a19de7fcd6a92b
sangzun-han <hsjun1996@naver.com>
2025-06-11 11:23:50 +0900

    fix : cart api response

--- src/entities/cart/api/get.ts
+++ src/entities/cart/api/get.ts
@@ -1,10 +1,11 @@
 import { api } from "@/shared/lib/api/ky";
-import { CartItem } from "../model";
+import { ApiResponse, CartItem } from "../model";
 import { BaseApiError, DisplayMode } from "@/shared/lib/api/errors/baseApiError";
 
 export const getCartItems = async (displayMode: DisplayMode = "fallback") => {
   try {
-    return await api.get("shopping-basket").json<CartItem[]>();
+    const response = await api.get("shopping-basket").json<ApiResponse<CartItem[]>>();
+    return response.data;
   } catch (err) {
     if (err instanceof BaseApiError) {
       err.displayMode = displayMode;
